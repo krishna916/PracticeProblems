d9160d11db1bbdd63a1d1a1776a1448ef060ef42
Krishnamurti Pandaram <krishna916@outlook.com>
2023-07-10 09:01:20 +0530

    Added Recursive binarySearch

--- src/main/java/com/krishnamurti/programmingTechniques/Recursion.java
+++ src/main/java/com/krishnamurti/programmingTechniques/Recursion.java
@@ -27,14 +27,57 @@ public class Recursion {
     }
 
 
+    public static int recursiveSummation (int inputNumber) {
+        // base case
+        if (inputNumber <= 1) {
+            return inputNumber;
+        }
+        // minimum logic
+        return inputNumber + recursiveSummation(inputNumber - 1);
+    }
+
+    /**
+     * recursive BinarySearch
+     * @param arr array to be operated upon
+     * @param left left index of current iteration
+     * @param right right index of current iteration
+     * @param x value to be searched
+     * @return index of the value if found. -1 if not found.
+     */
+    public static int binarySearch(int[] arr, int left, int right, int x)  {
+        // base case
+        if (left > right) {
+            return -1;
+        }
+
+        int mid = left + (right - left) / 2;
+
+        // base case: success case
+        if (arr[mid] == x) {
+            return mid;
+        }
+
+        if (x < arr[mid]) {
+            return binarySearch(arr, left, mid - 1, x);
+        }
+
+        return binarySearch(arr, mid + 1, right, x);
+    }
+
+
     public static void main(String[] args) {
 
         // String reverse
         System.out.println(stringReverse("hello"));
 
         // decimalToBinary
-
         System.out.println(decimalToBinary(233, ""));
 
+        // recursive summation
+        System.out.println(recursiveSummation(5));
+
+        int[] arr = new int[] { -1, 0, 4, 5, 6, 18, 45, 56, 89, 100 };
+        System.out.println(binarySearch(arr, 0, arr.length -1, 100));
+        System.out.println(binarySearch(arr, 0, arr.length -1, 99));
     }
 }
